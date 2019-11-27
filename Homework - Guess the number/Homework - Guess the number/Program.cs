3b9415404c0fecccf8d5d3ecf8f18d514ddea14f
palalaeclaudiu <claudiu.palalae@gosocialdev.eu>
2019-11-27 22:50:32 +0200

    Homework done.

--- Homework - Guess the number/Homework - Guess the number/Program.cs
+++ Homework - Guess the number/Homework - Guess the number/Program.cs
@@ -10,6 +10,26 @@ namespace Homework___Guess_the_number
     {
         static void Main(string[] args)
         {
+            Random r = new Random();
+            int g = r.Next(0, 100);
+
+            Console.WriteLine("Enter a number: ");
+            int n = int.Parse(Console.ReadLine());
+
+            while (n!=g)
+            {
+                if (n < g)
+                {
+                    Console.WriteLine("Number is too small. Enter another number: ");
+                    n = int.Parse(Console.ReadLine());
+                } else
+                {
+                    Console.WriteLine("Number is too big. Enter another number: ");
+                    n = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Yuhuu! You guessed the number");
         }
     }
 }
